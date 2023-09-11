36734f565e519cc42fc8ce3fff214520cd70495c
veryReallyGoodCoder <6000176@sae.edu.za>
2023-09-11 16:07:43 +0200

    Player Movement || Camera Move Added, Player Move Edit

---
+++ GrappleGame/Assets/Scripts/Player.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    
+    private Vector2 moveInput, mouseInput;
+    public float moveSpeed = 10f;
+
+    public Transform camera;
+    [SerializeField] private float lookSpeed = 10f;
+    private float xRotation = 0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+        Vector3 move = transform.right * moveInput.x + transform.forward * moveInput.y;
+        rb.MovePosition(transform.position + move * moveSpeed * Time.fixedDeltaTime);
+
+    }
+
+    public void MovePlayer(InputAction.CallbackContext ctx)
+    {
+        moveInput = ctx.ReadValue<Vector2>();
+    }
+
+    public void MouseScript(InputAction.CallbackContext ctx)
+    {
+        mouseInput = ctx.ReadValue < Vector2>() * lookSpeed;
+
+        xRotation -= mouseInput.y;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        camera.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        gameObject.transform.Rotate(Vector3.up * mouseInput.x);
+
+        Debug.Log(mouseInput);
+
+    }
+
+}
