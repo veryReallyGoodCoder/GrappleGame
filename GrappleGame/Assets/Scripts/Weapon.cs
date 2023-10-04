0e9ee2157a358a291fe821e2f7c8c5b51d7f934d
veryReallyGoodCoder <6000176@sae.edu.za>
2023-10-04 20:57:54 +0200

    Floating Islands Added

--- GrappleGame/Assets/Scripts/Weapon.cs
+++ GrappleGame/Assets/Scripts/Weapon.cs
@@ -1,13 +1,22 @@
+using Unity.IO.LowLevel.Unsafe;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    public string weaponName;
-    public int weaponDamage;
+    public string weaponName { get; set; }
+
+    public int damage;
+
+    public int block;
 
     public virtual void Attack()
     {
         Debug.Log("Attacking with " +  weaponName);
+    }
+
+    public virtual void Block()
+    {
+        Debug.Log("Bloacking with " + weaponName);
     }
 
 }
