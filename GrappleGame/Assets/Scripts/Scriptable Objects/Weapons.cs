938cce041c9b423dd152ec3226f19178db4ffec7
veryReallyGoodCoder <6000176@sae.edu.za>
2023-10-10 13:15:07 +0200

    Skybox Added

--- GrappleGame/Assets/Scripts/Scriptable Objects/Weapons.cs
+++ GrappleGame/Assets/Scripts/Scriptable Objects/Weapons.cs
@@ -1,8 +1,14 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = WeaponData)]
+[CreateAssetMenu(fileName = "WeaponStat", menuName = "WeaponStatData")]
 
 public class Weapons : ScriptableObject
 {
-    
+
+    public string weaponName;
+    public string weaponType;
+
+    public int weaponDamage;
+    public int weaponBlock;
+
 }
