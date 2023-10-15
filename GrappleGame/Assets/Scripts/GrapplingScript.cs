c662a5a1d38c24ea3b281a8e30cc5ed992587ab4
veryReallyGoodCoder <6000176@sae.edu.za>
2023-10-15 09:58:29 +0200

    Player Transform Fix

--- GrappleGame/Assets/Scripts/GrapplingScript.cs
+++ GrappleGame/Assets/Scripts/GrapplingScript.cs
@@ -35,6 +35,7 @@ public class GrapplingScript : MonoBehaviour
     void Start()
     {
         lr = GetComponent<LineRenderer>();
+        lr.useWorldSpace = true;
         //speedLines.SetActive(false);
     }
 
