2ddee19f1a1a745354bbb5ee9a23347b62710c61
veryReallyGoodCoder <6000176@sae.edu.za>
2023-11-26 20:48:17 +0200

    NewGrappleScript || Grapple Anim Update

--- GrappleGame/Assets/Scripts/NewGrappleScript.cs
+++ GrappleGame/Assets/Scripts/NewGrappleScript.cs
@@ -15,6 +15,12 @@ public class NewGrappleScript : MonoBehaviour
     [SerializeField] private int  maxGrapple;
 
     private bool shoot, jump;
+
+    [Header("LR Anim")]
+
+    [SerializeField] private int linePoints, waveCount, warpCount;
+    [SerializeField] private float waveSize, animSpeed;
+
     
     // Start is called before the first frame update
     void Start()
@@ -30,26 +36,21 @@ public class NewGrappleScript : MonoBehaviour
         shoot = player.shoot;
         jump = player.jump;
 
-        /* i tried uing the new input system but it updates the rope end point constantly and that's not what i want.
-        if (shoot)
-        {
-            DrawRope();
-        }
-        else
-        {
-            lr.enabled = false;
-        }*/
-
         if (Input.GetMouseButtonDown(0))
         {
             DrawRope();
+
+            //StartCoroutine(AnimateRope(gp));
         }
         else if(Input.GetMouseButtonUp(0))
         {
             lr.enabled = false;
         }
 
+
         lr.SetPosition(0, firePoint.position);
+        //StartCoroutine(AnimateRope());
+
     }
 
     private void DrawRope()
@@ -68,6 +69,51 @@ public class NewGrappleScript : MonoBehaviour
         lr.enabled = true;
         lr.SetPosition(0, firePoint.position);
         lr.SetPosition(1, gp);
+
+        StartCoroutine(AnimateRope());
+
+    }
+
+    private IEnumerator AnimateRope()
+    {
+        float startTime = Time.time;
+
+        Vector3 startPos = lr.GetPosition(0);
+        Vector3 endPos = lr.GetPosition(1);
+
+        Vector3 pos = startPos;
+        while(pos != endPos)
+        {
+            float time = (Time.time - startTime) / animSpeed;
+            pos = Vector3.Lerp(startPos, endPos, time);
+            lr.SetPosition(1, pos);
+            yield return null;
+            if(pos == endPos)
+            {
+                Debug.Log("end pos reached");
+            }
+        }
+    }
+
+    private void SetPoints(Vector3 targetPos, float percent)
+    {
+        Vector3 ropeEnd = Vector3.Lerp(transform.position, targetPos, percent);
+        float length = maxGrapple;
+
+        for(int i = 0; i < linePoints; i++)
+        {
+            float xPos = (float)i / linePoints * length;
+            float reversePercent = (1 - percent);
+
+            float amp = Mathf.Sin(reversePercent * warpCount * Mathf.PI);
+
+            float yPos = Mathf.Sin((float)waveCount * i / linePoints * 2 * Mathf.PI * reversePercent) * amp;
+
+            Vector2 pos = new Vector3(xPos, yPos);
+            lr.SetPosition(1, pos);
+        }
+
+        Debug.Log("draw");
     }
 
 }
